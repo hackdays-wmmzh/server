32b058dd5c1b89f20faba274471f5947aee8d866
Rijad Zuzo <rijad.zuzo@adcubum.com>
2019-11-30 10:55:19 +0100

    Add new demo data

--- src/main/java/com/wmmzh/backend/DemoData.java
+++ src/main/java/com/wmmzh/backend/DemoData.java
@@ -77,44 +77,44 @@ public class DemoData {
     private List<String> readPersons() {
         return Arrays.asList(
                 "{\n" +
-                        "    \"email\": \"richard.garyson@waynmanor.com\",\n" +
-                        "    \"vorname\": \"Richard\",\n" +
-                        "    \"nachname\": \"Grayson\",\n" +
-                        "    \"versichertennummer\": \"568.785.186\",\n" +
-                        "    \"svnr\": \"756.596.777.892.3\",\n" +
-                        "    \"geburtsdatum\": \"03.12.1990\",\n" +
-                        "    \"adresse\": {\n" +
-                        "        \"strasse\": \"Wayne Manor 2\",\n" +
-                        "        \"plz\": \"8280\",\n" +
-                        "        \"ort\": \"Kreuzlingen\",\n" +
-                        "        \"land\": \"Schweiz\"\n" +
-                        "    }\n" +
-                        "}",
-                "{\n" +
-                        "    \"email\": \"selina.kyle@waynmanor.com\",\n" +
-                        "    \"vorname\": \"Selina\",\n" +
-                        "    \"nachname\": \"Kyle\",\n" +
-                        "    \"versichertennummer\": \"785.349.712\",\n" +
-                        "    \"svnr\": \"756.697.254.041.5\",\n" +
-                        "    \"geburtsdatum\": \"20.08.1993\",\n" +
-                        "    \"adresse\": {\n" +
-                        "        \"strasse\": \"Wayne Manor 3\",\n" +
-                        "        \"plz\": \"8280\",\n" +
-                        "        \"ort\": \"Kreuzlingen\",\n" +
-                        "        \"land\": \"Schweiz\"\n" +
-                        "    }\n" +
-                        "}",
-                "{\n" +
-                        "    \"email\": \"bruce.wayne@waynmanor.com\",\n" +
-                        "    \"vorname\": \"Bruce\",\n" +
-                        "    \"nachname\": \"Wayne\",\n" +
+                        "    \"email\": \"emanuele.diquattro@adcubum.com\",\n" +
+                        "    \"vorname\": \"Emanuele\",\n" +
+                        "    \"nachname\": \"Diquattro \",\n" +
                         "    \"versichertennummer\": \"405.895.789\",\n" +
                         "    \"svnr\": \"756.123.456.789.8\",\n" +
-                        "    \"geburtsdatum\": \"05.07.1985\",\n" +
+                        "    \"geburtsdatum\": \"05.07.1983\",\n" +
                         "    \"adresse\": {\n" +
                         "        \"strasse\": \"Wayne Manor 1\",\n" +
-                        "        \"plz\": \"8280\",\n" +
-                        "        \"ort\": \"Kreuzlingen\",\n" +
+                        "        \"plz\": \"8180\",\n" +
+                        "        \"ort\": \"Bülach\",\n" +
+                        "        \"land\": \"Schweiz\"\n" +
+                        "    }\n" +
+                        "}",
+                "{\n" +
+                        "    \"email\": \"claudia.grolimund@adcubum.com\",\n" +
+                        "    \"vorname\": \"Claudia\",\n" +
+                        "    \"nachname\": \"Grolimund\",\n" +
+                        "    \"versichertennummer\": \"568.785.186\",\n" +
+                        "    \"svnr\": \"756.596.777.892.3\",\n" +
+                        "    \"geburtsdatum\": \"03.12.1978\",\n" +
+                        "    \"adresse\": {\n" +
+                        "        \"strasse\": \"Zürcherstrasse 464\",\n" +
+                        "        \"plz\": \"9015\",\n" +
+                        "        \"ort\": \"St. Gallen\",\n" +
+                        "        \"land\": \"Schweiz\"\n" +
+                        "    }\n" +
+                        "}",
+                "{\n" +
+                        "    \"email\": \"walter.meister@adcubum.com\",\n" +
+                        "    \"vorname\": \"Walter\",\n" +
+                        "    \"nachname\": \"Meister\",\n" +
+                        "    \"versichertennummer\": \"785.349.712\",\n" +
+                        "    \"svnr\": \"756.697.254.041.5\",\n" +
+                        "    \"geburtsdatum\": \"20.08.1969\",\n" +
+                        "    \"adresse\": {\n" +
+                        "        \"strasse\": \"Bahnhofplatz 1A\",\n" +
+                        "        \"plz\": \"8304\",\n" +
+                        "        \"ort\": \"Wallisellen\",\n" +
                         "        \"land\": \"Schweiz\"\n" +
                         "    }\n" +
                         "}"
@@ -125,78 +125,104 @@ public class DemoData {
         return Arrays.asList(
                 "{\n" +
                         "    \"policennummer\": \"4505.895.789.001\",\n" +
+                        "    \"versicherteperson\": \"405.895.789\",\n" +
                         "    \"beginn\": \"01.01.2018\",\n" +
                         "    \"waehrung\": \"CHF\",\n" +
+                        "    \"praemiegesamt\": \"298.70\",\n" +
                         "    \"grundversicherung\": {\n" +
                         "        \"name\": \"Home Med Simplx Basis\",\n" +
                         "        \"beschreibung\": \"OKP Hausarztmodell\",\n" +
-                        "        \"praemie\": \"231.70\",\n" +
+                        "        \"pramie\": \"298.70\",\n" +
                         "        \"franchise\": {\n" +
                         "            \"gesamt\": \"1500\",\n" +
                         "            \"bezahlt\": \"200\",\n" +
                         "            \"offen\": \"1300\"\n" +
+                        "        },\n" +
+                        "        \"selbstbehalt\": {\n" +
+                        "            \"bezahlt\": \"210\",\n" +
+                        "            \"max\": \"700\"\n" +
                         "        }\n" +
                         "    },\n" +
                         "    \"zusatzversicherungen\": [\n" +
                         "        {\n" +
                         "            \"name\": \"Travel World\",\n" +
                         "            \"beschreibung\": \"Reiseversicherung\",\n" +
-                        "            \"praemie\": \"12.60\"\n" +
+                        "            \"pramie\": \"12.60\"\n" +
                         "        },\n" +
                         "        {\n" +
                         "            \"name\": \"Denat Tutti\",\n" +
                         "            \"beschreibung\": \"Zahnzusatz\",\n" +
-                        "            \"praemie\": \"35.90\"\n" +
+                        "            \"pramie\": \"35.90\"\n" +
                         "        }\n" +
                         "    ],\n" +
-                        "    \"spitalversicherungen\": [\n" +
+                        "    \"spitalversicheurngen\": [\n" +
                         "        {\n" +
                         "            \"name\": \"Travel World\",\n" +
                         "            \"beschreibung\": \"Reiseversicherung\",\n" +
-                        "            \"praemie\": \"18.50\"\n" +
+                        "            \"pramie\": \"18.50\"\n" +
                         "        }\n" +
                         "    ]\n" +
-                        "}",
+                        "}\n",
                 "{\n" +
                         "    \"policennummer\": \"4505.895.789.001\",\n" +
+                        "    \"versicherteperson\": \"568.785.186\",\n" +
                         "    \"beginn\": \"01.01.2019\",\n" +
                         "    \"geburtsdatum\": \"05.07.1985\",\n" +
                         "    \"waehrung\": \"CHF\",\n" +
-                        "    \"praemiegesamt\": \"232.5\",\n" +
+                        "    \"praemiegesamt\": \"222.10\",\n" +
                         "    \"grundversicherung\": {\n" +
-                        "        \"name\": \"Home Med Simplx Basis\",\n" +
-                        "        \"beschreibung\": \"OKP Hausarztmodell\",\n" +
-                        "        \"praemie\": \"190.20\",\n" +
+                        "        \"name\": \"Simplx Telmed Eco\",\n" +
+                        "        \"beschreibung\": \"OKP Telemedizin\",\n" +
+                        "        \"pramie\": \"190.20\",\n" +
                         "        \"franchise\": {\n" +
                         "            \"gesamt\": \"2500\",\n" +
                         "            \"bezahlt\": \"850\",\n" +
                         "            \"offen\": \"1650\"\n" +
+                        "        },\n" +
+                        "        \"selbstbehalt\": {\n" +
+                        "            \"bezahlt\": \"512\",\n" +
+                        "            \"max\": \"700\"\n" +
                         "        }\n" +
                         "    },\n" +
                         "    \"zusatzversicherungen\": [\n" +
                         "        {\n" +
-                        "            \"name\": \"Justice League\",\n" +
-                        "            \"beschreibung\": \"Superhelden Unfall\",\n" +
-                        "            \"praemie\": \"42.30\"\n" +
+                        "            \"name\": \"Animal Protect\",\n" +
+                        "            \"beschreibung\": \"Hunde Rechtsschutzversicherung\",\n" +
+                        "            \"pramie\": \"31.90\"\n" +
                         "        }\n" +
                         "    ]\n" +
-                        "}",
+                        "}\n",
                 "{\n" +
                         "    \"policennummer\": \"4505.895.789.001\",\n" +
+                        "    \"versicherteperson\": \"568.785.186\",\n" +
                         "    \"beginn\": \"01.01.2019\",\n" +
-                        "    \"geburtsdatum\": \"05.07.1985\",\n" +
+                        "    \"geburtsdatum\": \"05.07.1965\",\n" +
                         "    \"waehrung\": \"CHF\",\n" +
-                        "    \"praemiegesamt\": \"130.80\",\n" +
+                        "    \"praemiegesamt\": \"551.40\",\n" +
+                        "    \"grundversicherung\": {\n" +
+                        "        \"name\": \"Simplx Deluxe All\",\n" +
+                        "        \"beschreibung\": \"OKP Grundversichung\",\n" +
+                        "        \"pramie\": \"420.60\",\n" +
+                        "        \"franchise\": {\n" +
+                        "            \"gesamt\": \"500\",\n" +
+                        "            \"bezahlt\": \"380\",\n" +
+                        "            \"offen\": \"120\"\n" +
+                        "        },\n" +
+                        "        \"selbstbehalt\": {\n" +
+                        "            \"bezahlt\": \"380\",\n" +
+                        "            \"max\": \"700\"\n" +
+                        "        }\n" +
+                        "    },\n" +
                         "    \"zusatzversicherungen\": [\n" +
                         "        {\n" +
-                        "            \"name\": \"Justice League\",\n" +
+                        "            \"name\": \"Avengers Accident\",\n" +
                         "            \"beschreibung\": \"Superhelden Unfall\",\n" +
-                        "            \"praemie\": \"42.30\"\n" +
+                        "            \"pramie\": \"42.30\"\n" +
                         "        },\n" +
                         "        {\n" +
                         "            \"name\": \"Protection Plus\",\n" +
                         "            \"beschreibung\": \"Rechtsschutz\",\n" +
-                        "            \"praemie\": \"88.50\"\n" +
+                        "            \"pramie\": \"88.50\"\n" +
                         "        }\n" +
                         "    ]\n" +
                         "}"
